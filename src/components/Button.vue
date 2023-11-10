b010559c0133aef96b69a0e71ad1077b35403334
injulme <injulme0309@naver.com>
2023-11-10 18:08:44 +0900

    first commit :)

---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+  text: String,
+  variant: "primary" | "secondary",
+});
+console.log("props => ", props.variant);
+</script>
+
+<template>
+  <button class="button">{{ text }}</button>
+</template>
+
+<style scoped>
+button {
+  padding: 12px 24px;
+  border: 2px solid yellow;
+  border-radius: 8px;
+  background-color: orange;
+  color: white;
+  font-size: 24px;
+  font-weight: bold;
+  cursor: pointer;
+}
+button:hover {
+  opacity: 0.8;
+}
+</style>
